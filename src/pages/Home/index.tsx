c07de5321760367c45841ef0d4427dd5319d37a7
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-10-08 11:10:26 -0300

    Iterando sobre o carrinho para exibir na Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -53,10 +53,11 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
+      {products.map(product => (
+        <li key={product.id}>
+        <img src={product.image} alt={product.title} />
+        <strong>{product.title}</strong>
+        <span>{ product.priceFormatted }</span>
         <button
           type="button"
           data-testid="add-product-button"
@@ -64,12 +65,13 @@ const Home = (): JSX.Element => {
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
-            {{cartItemsAmount[product.id] || 0}} 2
+            {cartItemsAmount[product.id] || 0} 
           </div>
 
           <span>ADICIONAR AO CARRINHO</span>
         </button>
       </li>
+      ))}
     </ProductList>
   );
 };
